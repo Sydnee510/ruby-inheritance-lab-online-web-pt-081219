3e2bb2517d42c3acf416552217c36d3d3e410b0f
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-21 14:37:30 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,7 @@
 class User 
 attr_accessor :first_name, :last_name 
 def initialize(firstname, lastname)
-  @first_name = firstname
-  @last_name = lastname
+  @first_name = first_name
+  @last_name = last_name
 end 
 end
